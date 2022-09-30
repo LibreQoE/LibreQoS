b941677f8f828d318b7019e48c6fd9f3461f3ab0
Robert Chacón <robertchacon@protonmail.com>
2022-09-30 04:54:20 -0600

    XDP enabled by default, remove usingXDP boolean

--- v1.2/ispConfig.example.py
+++ v1.2/ispConfig.example.py
@@ -20,10 +20,6 @@ interfaceA = 'eth1'
 # Interface connected to edge router
 interfaceB = 'eth2'
 
-# Use XDP? If yes, multiple CPU cores can be used. Limits to IPv4 only. Throughput of 11 Gbps+
-# If using IPv6, choose False. False will limit throughput to 3-6 Gbps
-usingXDP = True
-
 # Allow shell commands. False causes commands print to console only without being executed.
 # MUST BE ENABLED FOR PROGRAM TO FUNCTION
 enableActualShellCommands = True
