2027957df223713e59d6222b38edd6880d1f6cfc
Herbert Wolverson <herberticus@gmail.com>
2023-01-04 17:22:32 +0000

    Update build_rust.sh to include node manager
    Update the Rust build script to include the node manager,
    and its required files.
    
    Signed-off-by: Herbert Wolverson <herberticus@gmail.com>

--- src/build_rust.sh
+++ src/build_rust.sh
@@ -7,7 +7,7 @@
 # automatically.
 #
 # Don't forget to setup `/etc/lqos`
-PROGS="lqosd lqtop xdp_iphash_to_cpu_cmdline xdp_pping"
+PROGS="lqosd lqtop xdp_iphash_to_cpu_cmdline xdp_pping lqos_node_manager"
 mkdir -p bin/static
 pushd rust
 #cargo clean
@@ -24,5 +24,5 @@ do
 done
 popd
 cp -R rust/lqos_node_manager/static/* bin/static
-
+cp rust/lqos_node_manager/Rocket.toml bin/
 echo "Don't forget to setup /etc/lqos!"
