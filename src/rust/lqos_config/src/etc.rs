8afdc2e0d136f7991edde6d76418d6c94a08acb8
Herbert Wolverson <herberticus@gmail.com>
2023-01-05 15:03:48 +0000

    WIP - Basic interface and Bifrost config displayable.

--- src/rust/lqos_config/src/etc.rs
+++ src/rust/lqos_config/src/etc.rs
@@ -1,15 +1,15 @@
 use std::path::Path;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use anyhow::{Result, Error};
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct EtcLqos {
     pub lqos_directory: String,
     pub bridge: Option<BridgeConfig>,
     pub tuning: Option<Tunables>,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Tunables {
     pub stop_irq_balance: bool,
     pub netdev_budget_usecs: u32,
@@ -21,21 +21,21 @@ pub struct Tunables {
     pub disable_offload: Vec<String>,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct BridgeConfig {
     pub use_kernel_bridge: bool,
     pub interface_mapping: Vec<BridgeInterface>,
     pub vlan_mapping: Vec<BridgeVlan>,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct BridgeInterface {
     pub name: String,
     pub scan_vlans: bool,
     pub redirect_to: String,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct BridgeVlan {
     pub parent: String,
     pub tag: u32,
--- src/rust/lqos_node_manager/src/config_control.rs
+++ src/rust/lqos_node_manager/src/config_control.rs
@@ -1,9 +1,43 @@
-use rocket::fs::NamedFile;
-use crate::cache_control::{LongCache, NoCache};
+use default_net::get_interfaces;
+use lqos_config::{LibreQoSConfig, EtcLqos};
+use rocket::{fs::NamedFile, serde::json::Json};
+use crate::cache_control::NoCache;
 
 // Note that NoCache can be replaced with a cache option
 // once the design work is complete.
 #[get("/config")]
 pub async fn config_page<'a>() -> NoCache<Option<NamedFile>> {
     NoCache::new(NamedFile::open("static/config.html").await.ok())
+}
+
+#[get("/api/list_nics")]
+pub async fn get_nic_list<'a>() -> NoCache<Json<Vec<(String, String, String)>>> {
+    let mut result = Vec::new();
+    for eth in get_interfaces().iter() {
+        let mac = if let Some(mac) = &eth.mac_addr {
+            mac.to_string()
+        } else {
+            String::new()
+        };
+        result.push((
+            eth.name.clone(),
+            format!("{:?}", eth.if_type),
+            mac,  
+        ));
+    }
+    NoCache::new(Json(result))
+}
+
+#[get("/api/python_config")]
+pub async fn get_current_python_config() -> NoCache<Json<LibreQoSConfig>> {
+    let config = lqos_config::LibreQoSConfig::load().unwrap();
+    println!("{:#?}", config);
+    NoCache::new(Json(config))
+}
+
+#[get("/api/lqosd_config")]
+pub async fn get_current_lqosd_config() -> NoCache<Json<EtcLqos>> {
+    let config = lqos_config::EtcLqos::load().unwrap();
+    println!("{:#?}", config);
+    NoCache::new(Json(config))
 }
