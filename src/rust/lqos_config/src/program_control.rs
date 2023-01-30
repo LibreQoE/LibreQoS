e5500cf5283f6c733365485ad69a5f0a10109e7a
Herbert Wolverson <herberticus@gmail.com>
2023-01-30 22:10:54 +0000

    ISSUE #209: add detailed errors and logging to the program control system in lqos_config. Some errors should be impossible.

--- src/rust/lqos_config/src/program_control.rs
+++ src/rust/lqos_config/src/program_control.rs
@@ -1,5 +1,7 @@
+use log::error;
+use thiserror::Error;
+
 use crate::etc;
-use anyhow::{Error, Result};
 use std::{
     path::{Path, PathBuf},
     process::Command,
@@ -7,34 +9,51 @@ use std::{
 
 const PYTHON_PATH: &str = "/usr/bin/python3";
 
-fn path_to_libreqos() -> Result<PathBuf> {
-    let cfg = etc::EtcLqos::load()?;
+fn path_to_libreqos() -> Result<PathBuf, ProgramControlError> {
+    let cfg = etc::EtcLqos::load().map_err(|_| ProgramControlError::ConfigLoadError)?;
     let base_path = Path::new(&cfg.lqos_directory);
     Ok(base_path.join("LibreQoS.py"))
 }
 
-fn working_directory() -> Result<PathBuf> {
-    let cfg = etc::EtcLqos::load()?;
+fn working_directory() -> Result<PathBuf, ProgramControlError> {
+    let cfg = etc::EtcLqos::load().map_err(|_| ProgramControlError::ConfigLoadError)?;
     let base_path = Path::new(&cfg.lqos_directory);
     Ok(base_path.to_path_buf())
 }
 
 /// Shells out and reloads the `LibreQos.py` program, storing all
 /// emitted text and returning it.
-pub fn load_libreqos() -> Result<String> {
+pub fn load_libreqos() -> Result<String, ProgramControlError> {
     let path = path_to_libreqos()?;
     if !path.exists() {
-        return Err(Error::msg("LibreQoS.py not found"));
+        error!("Unable to locate LibreQoS.py. ({}) Check your configuration directory.", path.display());
+        return Err(ProgramControlError::LibreQosPyNotFound);
     }
     if !Path::new(PYTHON_PATH).exists() {
-        return Err(Error::msg("Python not found"));
+        error!("Unable to find Python binary ({PYTHON_PATH})");
+        return Err(ProgramControlError::PythonNotFound);
     }
 
     let result = Command::new(PYTHON_PATH)
         .current_dir(working_directory()?)
         .arg("LibreQoS.py")
-        .output()?;
-    let stdout = String::from_utf8(result.stdout)?;
-    let stderr = String::from_utf8(result.stderr)?;
+        .output()
+        .map_err(|_| ProgramControlError::CommandFailed)?;
+    let stdout = String::from_utf8(result.stdout).map_err(|_| ProgramControlError::StdInErrAccess)?;
+    let stderr = String::from_utf8(result.stderr).map_err(|_| ProgramControlError::StdInErrAccess)?;
     Ok(stdout + &stderr)
+}
+
+#[derive(Error, Debug)]
+pub enum ProgramControlError {
+    #[error("Unable to load lqos configuration from /etc")]
+    ConfigLoadError,
+    #[error("Unable to find LibreQoS.py")]
+    LibreQosPyNotFound,
+    #[error("Unable to find Python. Is it installed?")]
+    PythonNotFound,
+    #[error("Problem Invoking Command. This shouldn't happen.")]
+    CommandFailed,
+    #[error("Problem accessing stdin/stderr. This shouldn't happen")]
+    StdInErrAccess,
 }
