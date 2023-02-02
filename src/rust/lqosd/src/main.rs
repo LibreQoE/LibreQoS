30a6a2c5c5e48e7cf03544532e1071dadad0ab97
Herbert Wolverson <herberticus@gmail.com>
2023-02-02 14:08:20 +0000

    Revert my truly awful decision to try and run LibreQoS.py during lqosd startup (after checking that config files existed). It made startup unstable.

--- src/rust/lqosd/src/main.rs
+++ src/rust/lqosd/src/main.rs
@@ -103,23 +103,11 @@ async fn main() -> Result<()> {
     }
   });
 
+  // Create the socket server
+  let server = UnixSocketServer::new().expect("Unable to spawn server");
+
   // Main bus listen loop
-  let listener = async move {
-    // Create the socket server
-    let server = UnixSocketServer::new().expect("Unable to spawn server");
-    server.listen(handle_bus_requests).await
-  };
-  let handle = tokio::spawn(listener);
-
-  if lqos_config::LibreQoSConfig::config_exists() && lqos_config::ConfigShapedDevices::exists() {
-    warn!("Since all the files exist, Launching LibreQoS.py to avoid empty queues.");
-    program_control::reload_libre_qos();
-  } else {
-    warn!("ispConfig.py or ShapedDevices.csv hasn't been setup yet. Not automatically running LibreQoS.py");
-  }
-
-  info!("{:?}", handle.await?);
-
+  server.listen(handle_bus_requests).await?;
   Ok(())
 }
 
