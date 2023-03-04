2e50a2a108c94542c46775b428b9ae60767b7b98
Robert Chacón <robertchacon@protonmail.com>
2023-03-03 20:38:32 -0700

    Update integrationSplynx.py

--- src/integrationSplynx.py
+++ src/integrationSplynx.py
@@ -86,7 +86,7 @@ def createShaper():
 						id=combinedId,
 						displayName=customerJson["name"],
 						address=combineAddress(customerJson),
-						customerName = customerJson["name"]
+						customerName=customerJson["name"],
 						download=downloadForTariffID[tariff_id],
 						upload=uploadForTariffID[tariff_id],
 					)
