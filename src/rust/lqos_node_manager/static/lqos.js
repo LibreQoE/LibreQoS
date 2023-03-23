cf41b47b773cd4c10e2d74e970c7e70270861e2d
Herbert "TheBracket <herberticus@gmail.com>
2023-03-23 13:50:19 -0500

    Issue 288 - 0s showing up as dots, delays unit (#295)
    * Hide zero values for throughput, backlog and delays graphs
    
    Relates to ISSUE #288
    
    * New function `array_to_zero` to reduce repetition.
    * Transform zero values to `null` in Y value arrays for the
      throughput, backlog and delay graphs on circuit_info.html
    * Also change tin labelling to match the "backlog" graph on
      Delays.
    * Reduce marker size.
    
    * Scale displayed/graphed queue delays to ms
    
    References ISSUE 288
    
    Scale the delays graph entries, which are apparently in usec, to
    ms.

--- src/rust/lqos_node_manager/static/lqos.js
+++ src/rust/lqos_node_manager/static/lqos.js
@@ -388,3 +388,9 @@ function zip(a, b) {
     }
     return zipped;
 }
+
+function zero_to_null(array) {
+    for (let i=0; i<array.length; ++i) {
+        if (array[i] == 0) array[i] = null;
+    }
+}
