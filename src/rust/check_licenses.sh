c17d269ad657c3331d8e5b998402a395ad75a1e2
Herbert "TheBracket <herberticus@gmail.com>
2023-03-23 13:48:03 -0500

    Rust licenses (#292)
    * Add license specifier to all Cargo.toml files
    
    Explicitly specify `license = "GPL-2.0-only"` in each Cargo.toml
    file. The wording is from the SPX guidelines.
    
    * Add shell script to check licenses of Rust projects
    
    rust/check_licenses.sh automatically verifies all dependencies
    for accidental usage of GPLv3. Since basically nobody in the
    universe uses GPLv3, it tends not to do much.

---
+++ src/rust/check_licenses.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Checks all Rust projects for accidental inclusion of something GPL-3
+# licensed.
+
+PROJECTS="lqos_anonymous_stats_server lqos_bus lqos_config lqos_heimdall lqos_node_manager lqos_python lqos_queue_tracker lqos_setup lqos_sys lqos_utils lqosd lqusers xdp_iphash_to_cpu_cmdline xdp_pping"
+TOOL="cargo license --help"
+
+# Check that the tool exists
+if ! $TOOL &> /dev/null
+then
+    echo "Cargo License Tool not Found. Installing it."
+    cargo install cargo-license
+fi
+
+# Check every project
+for project in $PROJECTS
+do
+    pushd $project > /dev/null
+    if cargo license | grep "GPL-3"; then
+        echo "Warning: GPL3 detected in dependencies for $project"
+    fi
+    popd > /dev/null
+done
