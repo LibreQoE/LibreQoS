80c9a279b349f763e368e1e3da05ad4f04c7e3a8
Herbert Wolverson <herberticus@gmail.com>
2023-04-06 19:01:03 +0000

    Include new directories

---
+++ src/rust/long_term_stats/README.md
@@ -0,0 +1,3 @@
+# Long Term Stats
+
+Details go here.
---
+++ src/rust/long_term_stats/license_server/src/main.rs
@@ -0,0 +1,13 @@
+mod server;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+  // Start the logger
+  env_logger::init_from_env(
+    env_logger::Env::default()
+      .filter_or(env_logger::DEFAULT_FILTER_ENV, "warn"),
+  );
+
+  let _ = server::start().await;
+  Ok(())
+}
