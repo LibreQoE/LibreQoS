c0aca98cdfb7170733c6f184449b09f9e195e693
Herbert Wolverson <herberticus@gmail.com>
2023-05-04 19:06:48 +0000

    Working site heatmap on root page.

--- src/rust/long_term_stats/site_build/src/components/root_heat.ts
+++ src/rust/long_term_stats/site_build/src/components/root_heat.ts
@@ -1,17 +1,102 @@
 import { Component } from "./component";
+import * as echarts from 'echarts';
 
 export class RootHeat implements Component {
+    div: HTMLElement;
+    myChart: echarts.ECharts;
+    counter: number = 0;
+
+    constructor() {
+        this.div = document.getElementById("rootHeat") as HTMLElement;
+        this.myChart = echarts.init(this.div);
+        this.myChart.showLoading();
+    }
+
     wireup(): void {
-        
+        window.bus.requestSiteRootHeat();        
     }
 
     ontick(): void {
-        window.bus.requestSiteRootHeat();
+        this.counter++;
+        if (this.counter % 10 == 0)
+            window.bus.requestSiteRootHeat();        
     }
 
     onmessage(event: any): void {
         if (event.msg == "rootHeat") {
-            console.log(event);
+            this.myChart.hideLoading();
+
+            let categories: string[] = [];
+            let x: string[] = [];
+            let first: boolean = true;
+            let count = 0;
+            let data: any[] = [];
+            let keys: string[] = [];
+            for (const key in event.data) {
+                keys.push(key);
+            }
+            keys = keys.sort().reverse();
+            console.log(keys);
+
+            for (let j=0; j<keys.length; j++) {
+                let key = keys[j];
+                categories.push(key);
+
+                // Push the X axis values
+                if (first) {
+                    first = false;
+                    for (let i=0; i<event.data[key].length; i++) {
+                        x.push(event.data[key][i][0]);
+                    }
+                }
+
+                // Create all the series entries for this category
+                for (let i=0; i<event.data[key].length; i++) {
+                    data.push([i, count, event.data[key][i][1]]);
+                }
+
+                count++;
+            }
+
+            let series: any[] = [];
+            let i = 0;
+            series.push({
+                name: categories[i],
+                type: 'heatmap',
+                data: data,
+                label: { show: true },
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                    }
+            })
+            //console.log(series);
+
+            let option = {
+                title: { text: "TCP Round-Trip Time by Site" },
+                tooltip: {
+                    show: false,
+                  },
+                grid: { height: '50%', top: '10%' },
+                xAxis: { type: 'category', data: x, splitArea: { show: true } },
+                yAxis: { type: 'category', data: categories, splitArea: { show: true } },
+                series: series,
+                visualMap: {
+                    min: 0,
+                    max: 200,
+                    calculable: true,
+                    type: 'continuous',
+                    orient: 'horizontal',
+                    left: 'center',
+                    top: '2%',
+                    inRange : {   
+                        color: ['#009000', 'yellow', '#DD2000' ] //From smaller to bigger value ->
+                    }
+                  },
+            };
+            this.myChart.setOption(option);
         }
     }
 }
