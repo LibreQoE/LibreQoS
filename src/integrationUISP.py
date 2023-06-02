8ea5d4bdbca69e5744182480e6dfe2207fc4568c
Robert Chacón <robertchacon@protonmail.com>
2023-06-02 11:04:56 -0600

    Update integrationUISP.py

--- src/integrationUISP.py
+++ src/integrationUISP.py
@@ -10,6 +10,10 @@ try:
 except:
 	from ispConfig import uispSite, uispStrategy
 	overwriteNetworkJSONalways = False
+try:
+	from ispConfig import airMax_capacity
+except:
+	airMax_capacity = 0.65
 
 def uispRequest(target):
 	# Sends an HTTP request to UISP and returns the
@@ -136,6 +140,9 @@ def findApCapacities(devices, siteBandwidth):
 				download = int(device['overview']
 							   ['downlinkCapacity'] / 1000000)
 				upload = int(device['overview']['uplinkCapacity'] / 1000000)
+				dlRatio = None
+				if device['identification']['type'] == 'airMax':
+					download, upload = airMaxCapacityCorrection(device, download, upload)
 				if (download < 15) or (upload < 15):
 					print("WARNING: Device '" + device['identification']['hostname'] + "' has unusually low capacity (" + str(download) + '/' + str(upload) + " Mbps). Discarding in favor of parent site rates.")
 					safeToUse = False
@@ -147,6 +154,29 @@ def findApCapacities(devices, siteBandwidth):
 					siteBandwidth[device['identification']['name']] = {
 						"download": download, "upload": upload}
 
+def airMaxCapacityCorrection(device, download, upload):
+	dlRatio = None
+	for interface in device['interfaces']:
+		if ('wireless' in interface) and (interface['wireless'] != None):
+			if 'dlRatio' in interface['wireless']:
+				dlRatio = interface['wireless']['dlRatio']
+	# UISP reports unrealistically high capacities for airMax.
+	# For fixed frame, multiply capacity by the ratio for download/upload.
+	# For Flexible Frame, use 65% of reported capcity.
+	# 67/33
+	if dlRatio == 67:
+		download = download * 0.67
+		upload = upload * 0.33
+	# 50/50
+	elif dlRatio == 50:
+		download = download * 0.50
+		upload = upload * 0.50
+	# Flexible frame
+	elif dlRatio == None:
+		download = download * airMax_capacity
+		upload = upload * airMax_capacity
+	return (download, upload)
+
 def findAirfibers(devices, generatedPNDownloadMbps, generatedPNUploadMbps):
 	foundAirFibersBySite = {}
 	for device in devices:
@@ -157,6 +187,9 @@ def findAirfibers(devices, generatedPNDownloadMbps, generatedPNUploadMbps):
 						if device['overview']['downlinkCapacity'] is not None and device['overview']['uplinkCapacity'] is not None:
 							download = int(device['overview']['downlinkCapacity']/ 1000000)
 							upload = int(device['overview']['uplinkCapacity']/ 1000000)
+							# Correct AirMax Capacities
+							if device['identification']['type'] == 'airMax':
+								download, upload = airMaxCapacityCorrection(device, download, upload)
 							# Make sure to factor in gigabit port for AF60/AF60-LRs
 							if (device['identification']['model'] == "AF60-LR") or (device['identification']['model'] == "AF60"):
 								download = min(download,950)
