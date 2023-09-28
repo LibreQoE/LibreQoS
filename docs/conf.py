b703c032900de71c921e2639d394d0f86cc69e6b
Robert Chacón <robertchacon@protonmail.com>
2023-09-28 09:09:03 -0600

    Delete docs/conf.py

--- docs/conf.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-project = 'LibreQoS'
-copyright = '2023, LibreQoE, LLC'
-version = '1.4'
-release = 'version 1.4'
-highlight_language = 'python'
-pygments_style = 'sphinx'
-needs_sphinx = '1.3'
-master_doc = 'index'
