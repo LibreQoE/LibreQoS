67dfb60575427ad7e814bbadea474cfb2c0c419f
Robert Chacón <robertchacon@protonmail.com>
2023-09-28 09:03:37 -0600

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+import sys, os
+project = 'LibreQoS'
+copyright = '2023, LibreQoE, LLC'
+version = '1.4'
+release = 'version 1.4'
+highlight_language = 'python'
+pygments_style = 'sphinx'
+needs_sphinx = '1.3'
