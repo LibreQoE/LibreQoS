fcf07db1126368ba9a8ce049c72ebb165af31be6
Herbert Wolverson <herberticus@gmail.com>
2024-03-01 12:12:32 -0600

    On flat networks, read calculated LTS (or not) weights and apply them to the binpacking circuit weights if found. This makes binpacking more dynamic for flat networks, taking into account usual usage patterns.

--- src/LibreQoS.py
+++ src/LibreQoS.py
@@ -25,7 +25,7 @@ from liblqos_python import is_lqosd_alive, clear_ip_mappings, delete_ip_mapping,
 	check_config, sqm, upstream_bandwidth_capacity_download_mbps, upstream_bandwidth_capacity_upload_mbps, \
 	interface_a, interface_b, enable_actual_shell_commands, use_bin_packing_to_balance_cpu, monitor_mode_only, \
 	run_shell_commands_as_sudo, generated_pn_download_mbps, generated_pn_upload_mbps, queues_available_override, \
-	on_a_stick, get_tree_weights
+	on_a_stick, get_tree_weights, get_weights
 
 # Automatically account for TCP overhead of plans. For example a 100Mbps plan needs to be set to 109Mbps for the user to ever see that result on a speed test
 # Does not apply to nodes of any sort, just endpoint devices
@@ -513,6 +513,13 @@ def refreshShapers():
 			generatedPNs.append(genPNname)
 		if use_bin_packing_to_balance_cpu():
 			print("Using binpacking module to sort circuits by CPU core")
+			weights = get_weights()
+			for circuitId in dictForCircuitsWithoutParentNodes:
+				for circuit in subscriberCircuits:
+					if circuit['idForCircuitsWithoutParentNodes'] == circuitId:
+						for w in weights:
+							if w.circuit_id == circuit['circuitID']:
+								dictForCircuitsWithoutParentNodes[circuitId] = w.weight
 			bins = binpacking.to_constant_bin_number(dictForCircuitsWithoutParentNodes, numberOfGeneratedPNs)
 			genPNcounter = 0
 			for binItem in bins:
