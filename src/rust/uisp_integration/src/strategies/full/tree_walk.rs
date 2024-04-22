d7181a663df9de1400f6769fea203bebfb6482a6
Herbert Wolverson <herberticus@gmail.com>
2024-04-22 11:14:20 -0500

    Support routing overrides

--- src/rust/uisp_integration/src/strategies/full/tree_walk.rs
+++ src/rust/uisp_integration/src/strategies/full/tree_walk.rs
@@ -1,15 +1,16 @@
 use crate::errors::UispIntegrationError;
-use crate::strategies::full::walk_node;
+use crate::strategies::full::routes_override::RouteOverride;
 use crate::uisp_types::{UispSite, UispSiteType};
 
 pub fn walk_tree_for_routing(
     sites: &mut Vec<UispSite>,
     root_site: &str,
+    overrides: &Vec<RouteOverride>,
 ) -> Result<(), UispIntegrationError> {
     if let Some(root_idx) = sites.iter().position(|s| s.name == root_site) {
         let mut visited = std::collections::HashSet::new();
         let mut current_node = root_idx;
-        walk_node(current_node, 10, sites, &mut visited);
+        walk_node(current_node, 10, sites, &mut visited, overrides);
     } else {
         tracing::error!("Unable to build a path-weights graph because I can't find the root node");
         return Err(UispIntegrationError::NoRootSite);
@@ -25,4 +26,29 @@ pub fn walk_tree_for_routing(
     }
 
     Ok(())
+}
+
+fn walk_node(
+    idx: usize,
+    weight: u32,
+    sites: &mut Vec<UispSite>,
+    visited: &mut std::collections::HashSet<usize>,
+    overrides: &Vec<RouteOverride>,
+) {
+    if visited.contains(&idx) {
+        return;
+    }
+    visited.insert(idx);
+    for i in 0..sites.len() {
+        if sites[i].parent_indices.contains(&idx) {
+            let from = sites[i].name.clone();
+            let to = sites[idx].name.clone();
+            if let Some(route_override) = overrides.iter().find(|o| o.from_site == from && o.to_site == to) {
+                sites[i].route_weights.push((idx, route_override.cost));
+            } else {
+                sites[i].route_weights.push((idx, weight));
+            }
+            walk_node(i, weight + 10, sites, visited, overrides);
+        }
+    }
 }
