4f0c07c995c75eec4297a6444221a6f5f9b151cb
Herbert Wolverson <herberticus@gmail.com>
2024-06-21 12:33:26 -0500

    5 min throughput start

---
+++ src/rust/lqosd/src/node_manager/js_build/src/graphs/throughput_ring_graph.js
@@ -0,0 +1,119 @@
+import {DashboardGraph} from "./dashboard_graph";
+import {scaleNumber} from "../scaling";
+
+export class ThroughputRingBufferGraph extends DashboardGraph {
+    constructor(id) {
+        super(id);
+        this.ringbuffer = new RingBuffer(600);
+
+        let xaxis = [];
+        for (let i=0; i<600; i++) {
+            xaxis.push(i);
+        }
+
+        this.option = {
+            xAxis: {
+                type: 'category',
+                data: xaxis,
+            },
+            yAxis: {
+                type: 'value',
+                axisLabel: {
+                    formatter: (val) => {
+                        return scaleNumber(Math.abs(val));
+                    },
+                }
+            },
+            series: [
+                {
+                    name: 'shaped0',
+                    data: [],
+                    type: 'line',
+                    stack: 'shaped',
+                    lineStyle: {
+                        opacity: 0,
+                    },
+                    symbol: 'none',
+                },
+                {
+                    name: 'shaped1',
+                    data: [],
+                    type: 'line',
+                    stack: 'shaped',
+                    lineStyle: {
+                        opacity: 0,
+                    },
+                    symbol: 'none',
+                    areaStyle: {
+                        color: 'green'
+                    }
+                },
+                {
+                    name: 'unshaped0',
+                    data: [],
+                    type: 'line',
+                },
+                {
+                    name: 'unshaped1',
+                    data: [],
+                    type: 'line',
+                },
+            ],
+            tooltip: {
+                trigger: 'item',
+            },
+        }
+        this.option && this.chart.setOption(this.option);
+    }
+
+    update(shaped, unshaped) {
+        this.chart.hideLoading();
+        this.ringbuffer.push(shaped, unshaped);
+
+        let data = this.ringbuffer.series();
+        this.option.series[0].data = data[0];
+        this.option.series[1].data = data[1];
+        this.option.series[2].data = data[2];
+        this.option.series[3].data = data[3];
+
+        this.chart.setOption(this.option);
+    }
+}
+
+class RingBuffer {
+    constructor(size) {
+        this.size = size;
+        let data = [];
+        for (let i=0; i<size; i++) {
+            data.push([0, 0, 0, 0]);
+        }
+        this.head = 0;
+        this.data = data;
+    }
+
+    push(shaped, unshaped) {
+        this.data[this.head][0] = 0.0 - shaped[0];
+        this.data[this.head][1] = shaped[1];
+        this.data[this.head][2] = 0.0 - unshaped[0];
+        this.data[this.head][3] = unshaped[1];
+        this.head += 1;
+        this.head %= this.size;
+    }
+
+    series() {
+        let result = [
+            [], [], [], []
+        ];
+        for (let i=this.head; i<this.size; i++) {
+            for (let j=0; j<4; j++) {
+                result[j].push(this.data[i][j]);
+            }
+        }
+        for (let i=0; i<this.head; i++) {
+            for (let j=0; j<4; j++) {
+                result[j].push(this.data[i][j]);
+            }
+        }
+        return result;
+    }
+}
--- src/rust/lqosd/src/node_manager/js_build/src/index.js
+++ src/rust/lqosd/src/node_manager/js_build/src/index.js
@@ -2,10 +2,12 @@ import {subscribeWS} from "./pubsub/ws";
 import {BitsPerSecondGauge} from "./graphs/bits_gauge";
 import {PacketsPerSecondBar} from "./graphs/packets_bar";
 import {ShapedUnshapedPie} from "./graphs/shaped_unshaped_pie";
+import {ThroughputRingBufferGraph} from "./graphs/throughput_ring_graph";
 
 let tpBits = null;
 let tpPackets = null;
 let tpShaped = null;
+let tpRing = null;
 
 function onMessage(msg) {
     switch (msg.event) {
@@ -13,7 +15,8 @@ function onMessage(msg) {
             if (msg.channel === "throughput") {
                 tpBits = new BitsPerSecondGauge("tpBits");
                 tpPackets = new PacketsPerSecondBar("tpPackets");
-                tpShaped = new ShapedUnshapedPie("tpShaped")
+                tpShaped = new ShapedUnshapedPie("tpShaped");
+                tpRing = new ThroughputRingBufferGraph("tpRing");
             }
         }
             break;
@@ -21,6 +24,7 @@ function onMessage(msg) {
             tpBits.update(msg.data.bps[0], msg.data.bps[1], msg.data.max[0], msg.data.max[1]);
             tpPackets.update(msg.data.pps[0], msg.data.pps[1]);
             tpShaped.update(msg.data.shaped_bps[0] + msg.data.shaped_bps[1], msg.data.bps[0] + msg.data.bps[1]);
+            tpRing.update(msg.data.shaped_bps, msg.data.bps);
         }
             break;
     }
