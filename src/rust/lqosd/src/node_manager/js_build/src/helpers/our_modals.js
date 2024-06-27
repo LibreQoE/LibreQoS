25824d4da6e23945e415e0b0d10303631dac9a56
Herbert Wolverson <herberticus@gmail.com>
2024-06-27 08:47:57 -0500

    Modal system respects day/night mode.

--- src/rust/lqosd/src/node_manager/js_build/src/helpers/our_modals.js
+++ src/rust/lqosd/src/node_manager/js_build/src/helpers/our_modals.js
@@ -2,29 +2,14 @@
 export function darkBackground(id) {
     let darken = document.createElement("div");
     darken.id = id;
-    darken.style.zIndex = 200;
-    darken.style.position = "absolute";
-    darken.style.top = "0px";
-    darken.style.bottom = "0px";
-    darken.style.left = "0px";
-    darken.style.right = "0px";
-    darken.style.background = "rgba(1, 1, 1, 0.75)";
+    darken.classList.add("darkenBackground")
     return darken;
 }
 
 // Creates a div that sits happily atop the window
 export function modalContent(closeTargetId) {
     let content = document.createElement("div");
-    content.style.zIndex = 210;
-    content.style.position = "absolute";
-    content.style.top = "10%";
-    content.style.bottom = "10%";
-    content.style.left = "10%";
-    content.style.right = "10%";
-    //content.style.maxWidth = "500px";
-    //content.style.maxHeight = "500px";
-    content.style.background = "#eee";
-    content.style.padding = "10px";
+    content.classList.add("myModal");
     content.appendChild(closeButton(closeTargetId));
     return content;
 }
