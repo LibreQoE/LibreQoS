671e4314fbdfb52c6ca2c3a5263596483eabf8a3
Herbert Wolverson <herberticus@gmail.com>
2024-07-10 13:53:48 -0500

    Add a few more sponsorship messages.

--- src/rust/lqosd/src/node_manager/js_build/src/toasts/sponsor_us.js
+++ src/rust/lqosd/src/node_manager/js_build/src/toasts/sponsor_us.js
@@ -1,16 +1,38 @@
+const sponsorBtn = "<a href=\"https://github.com/sponsors/LibreQoE/\" target='_blank' class='btn btn-sm btn-outline-info'><i class=\"fa fa-heart\"></i> Sponsor Us on GitHub</a>";
+
 export function sponsorTag(parentId) {
     $.get("/local-api/ltsCheck", (data) => {
-        //console.log(data);
-        if (data.action !== "GoodToGo") {
-            let div = document.createElement("div");
-            let html = "We love working on LibreQoS to make the Internet better. If you love it too, please ";
-            html += "<a href=\"https://github.com/sponsors/LibreQoE/\" target='_blank' class='btn btn-sm btn-outline-info'>";
-            html += "<i class=\"fa fa-heart\"></i> Sponsor Us on GitHub</a>";
-            html += ". By the way, we'll stop asking if you sign up for LTS (Long-Term Stats).";
-            div.innerHTML = html;
-            div.classList.add("alert", "alert-warning");
-            let parent = document.getElementById(parentId);
-            parent.appendChild(div);
-        }
+        $.get("/local-api/deviceCount", (counts) => {
+            //console.log(data);
+            if (data.action !== "GoodToGo") {
+                let div = document.createElement("div");
+                let random = Math.floor(Math.random() * 4) + 1;
+                if (random === 1) {
+                    let html = "We love working on LibreQoS to make the Internet better. If you love it too, please ";
+                    html += sponsorBtn;
+                    html += ". By the way, we'll stop asking if you sign up for LTS (Long-Term Stats).";
+                    div.innerHTML = html;
+                } else if (random === 2) {
+                    let cost = Math.max(100, counts.shaped_devices) * 0.6;
+                    let html = "Other QoS providers might be charging you as much as $" + cost.toFixed(2) + " per month. We like eating, too! Why not ";
+                    html += sponsorBtn;
+                    html += ". By the way, we'll stop asking if you sign up for LTS (Long-Term Stats).";
+                    div.innerHTML = html;
+                } else if (random === 3) {
+                    let html = "Open Source is a labor of love. If you love LibreQoS, please ";
+                    html += sponsorBtn;
+                    html += ". By the way, we'll stop asking if you sign up for LTS (Long-Term Stats).";
+                    div.innerHTML = html;
+                } else if (random === 4) {
+                    let html = counts.shaped_devices + " devices on your network are using LibreQoS. If we're helping, please ";
+                    html += sponsorBtn;
+                    html += ". By the way, we'll stop asking if you sign up for LTS (Long-Term Stats).";
+                    div.innerHTML = html;
+                }
+                div.classList.add("alert", "alert-warning");
+                let parent = document.getElementById(parentId);
+                parent.appendChild(div);
+            }
+        });
     });
 }
