4e53a0e32cc7a31b8ce533a12692ecf1aa734b79
Herbert Wolverson <herberticus@gmail.com>
2024-07-11 14:20:34 -0500

    Tweak formatting on top 10 downloaders, and add a placeholder link to the circuit page.

--- src/rust/lqosd/src/node_manager/js_build/src/dashlets/top10_downloaders.js
+++ src/rust/lqosd/src/node_manager/js_build/src/dashlets/top10_downloaders.js
@@ -1,7 +1,7 @@
 import {BaseDashlet} from "./base_dashlet";
 import {RttHistogram} from "../graphs/rtt_histo";
 import {clearDashDiv, theading} from "../helpers/builders";
-import {scaleNumber, rttCircleSpan, formatThroughput, formatRtt} from "../helpers/scaling";
+import {scaleNumber, rttCircleSpan, formatThroughput, formatRtt, formatRetransmit} from "../helpers/scaling";
 import {redactCell} from "../helpers/redact";
 
 export class Top10Downloaders extends BaseDashlet {
@@ -37,30 +37,38 @@ export class Top10Downloaders extends BaseDashlet {
             let target = document.getElementById(this.id);
 
             let t = document.createElement("table");
-            t.classList.add("table", "table-striped", "tiny");
+            t.classList.add("table", "table-striped", "table-sm");
 
             let th = document.createElement("thead");
-            th.appendChild(theading(""));
             th.appendChild(theading("IP Address/Circuit"));
+            th.appendChild(theading("Plan"));
             th.appendChild(theading("DL ⬇️"));
             th.appendChild(theading("UL ⬆️"));
             th.appendChild(theading("RTT (ms)"));
-            th.appendChild(theading("TCP Retransmits"));
-            th.appendChild(theading("Shaped"));
+            th.appendChild(theading("TCP Retransmits", 2));
             t.appendChild(th);
 
             let tbody = document.createElement("tbody");
             msg.data.forEach((r) => {
                 let row = document.createElement("tr");
 
-                let circle = document.createElement("td");
-                circle.appendChild(rttCircleSpan(r.median_tcp_rtt));
-                row.appendChild(circle);
+                if (r.circuit_id !== "") {
+                    let link = document.createElement("a");
+                    link.href = "circuit.html?id=" + encodeURI(r.circuit_id);
+                    link.innerText = r.ip_address;
+                    redactCell(link);
+                    row.append(link);
+                } else {
+                    let ip = document.createElement("td");
+                    ip.innerText = r.ip_address;
+                    redactCell(ip);
+                    row.append(ip);
+                }
 
-                let ip = document.createElement("td");
-                ip.innerText = r.ip_address;
-                redactCell(ip);
-                row.append(ip);
+                let shaped = document.createElement("td");
+                shaped.classList.add("tiny");
+                shaped.innerText = r.plan.down + " / " + r.plan.up;
+                row.append(shaped);
 
                 let dl = document.createElement("td");
                 dl.innerHTML = formatThroughput(r.bits_per_second.down, r.plan.down);
@@ -74,13 +82,13 @@ export class Top10Downloaders extends BaseDashlet {
                 rtt.innerHTML = formatRtt(r.median_tcp_rtt);
                 row.append(rtt);
 
-                let tcp_xmit = document.createElement("td");
-                tcp_xmit.innerText = r.tcp_retransmits.down + " / " + r.tcp_retransmits.up;
-                row.append(tcp_xmit);
+                let tcp_xmit_down = document.createElement("td");
+                tcp_xmit_down.innerHTML = formatRetransmit(r.tcp_retransmits.down);
+                row.append(tcp_xmit_down);
 
-                let shaped = document.createElement("td");
-                shaped.innerText = r.plan.down + " / " + r.plan.up;
-                row.append(shaped);
+                let tcp_xmit_up = document.createElement("td");
+                tcp_xmit_up.innerHTML = formatRetransmit(r.tcp_retransmits.up);
+                row.append(tcp_xmit_up);
 
                 t.appendChild(row);
             });
