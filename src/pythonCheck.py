fe938b4a47eeb110fee8124488f990eed161b810
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2024-07-12 15:44:59 +0200

    pythonCheck.py: small code improvement

--- src/pythonCheck.py
+++ src/pythonCheck.py
@@ -4,11 +4,6 @@ def checkPythonVersion():
     # Perform a version check
     import sys
     print("Running Python Version " + sys.version)
-    version_ok = True
-    if sys.version_info[0] < 3:
-        version_ok = False
-    if sys.version_info[0]==3 and sys.version_info[1] < 10:
-        version_ok = False
-    if version_ok == False:
+    if sys.version_info[0] < 3 or (sys.version_info[0]==3 and sys.version_info[1] < 10):
         print("LibreQoS requires Python 3.10 or greater.")
         os._exit(-1)
