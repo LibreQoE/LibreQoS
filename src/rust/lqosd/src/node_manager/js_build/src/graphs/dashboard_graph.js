ec557dee78f1fec9ced35a7efe6e1c503efb9074
Herbert Wolverson <herberticus@gmail.com>
2024-07-20 09:13:17 -0500

    Dark/Light now toggles graphs correctly.

--- src/rust/lqosd/src/node_manager/js_build/src/graphs/dashboard_graph.js
+++ src/rust/lqosd/src/node_manager/js_build/src/graphs/dashboard_graph.js
@@ -2,11 +2,9 @@ import {isDarkMode} from "../helpers/dark_mode";
 
 export class DashboardGraph {
     constructor(id) {
-        let theme = "macarons";
-        if (isDarkMode()) theme = "dark";
         this.id = id;
         this.dom = document.getElementById(id);
-        this.chart = echarts.init(this.dom, theme);
+        this.chart = echarts.init(this.dom);
         this.chart.showLoading();
         this.option = {};
     }
