6cd40065da61acb2f7578ecc99091ebc952d5288
Herbert Wolverson <herberticus@gmail.com>
2024-07-23 08:49:38 -0500

    Robert's excellent dashboard template is now the default if you don't have one defined.

--- src/rust/lqosd/src/node_manager/js_build/src/dashlets/dashboard.js
+++ src/rust/lqosd/src/node_manager/js_build/src/dashlets/dashboard.js
@@ -711,7 +711,7 @@ class Layout {
         if (template !== null) {
             this.dashlets = JSON.parse(template);
         } else {
-            this.dashlets = JSON.parse(DASHBOARD_LIKE_V14);
+            this.dashlets = JSON.parse(ROBERT_DASHBOARD);
         }
     }
 
@@ -722,4 +722,4 @@ class Layout {
     }
 }
 
-const DASHBOARD_LIKE_V14 = "[{\"name\":\"Throughput Bits/Second\",\"tag\":\"throughputBps\",\"size\":3},{\"name\":\"Throughput Packets/Second\",\"tag\":\"throughputPps\",\"size\":3},{\"name\":\"Shaped/Unshaped Pie\",\"tag\":\"shapedUnshaped\",\"size\":3},{\"name\":\"Tracked Flows Counter\",\"tag\":\"trackedFlowsCount\",\"size\":3},{\"name\":\"Last 5 Minutes Throughput\",\"tag\":\"throughputRing\",\"size\":6},{\"name\":\"Round-Trip Time Histogram\",\"tag\":\"rttHistogram\",\"size\":6},{\"name\":\"Combined Top 10 Box\",\"tag\":\"combinedTop10\",\"size\":6},{\"name\":\"Network Tree Summary\",\"tag\":\"treeSummary\",\"size\":6}]";
+const ROBERT_DASHBOARD = "[{\"name\":\"Throughput Bits/Second\",\"tag\":\"throughputBps\",\"size\":2},{\"name\":\"Shaped/Unshaped Pie\",\"tag\":\"shapedUnshaped\",\"size\":2},{\"name\":\"Throughput Packets/Second\",\"tag\":\"throughputPps\",\"size\":2},{\"name\":\"Tracked Flows Counter\",\"tag\":\"trackedFlowsCount\",\"size\":2},{\"name\":\"Last 5 Minutes Throughput\",\"tag\":\"throughputRing\",\"size\":2},{\"name\":\"Round-Trip Time Histogram\",\"tag\":\"rttHistogram\",\"size\":2},{\"name\":\"Network Tree Sankey\",\"tag\":\"networkTreeSankey\",\"size\":6},{\"name\":\"Network Tree Summary\",\"tag\":\"treeSummary\",\"size\":6},{\"name\":\"Top 10 Downloaders\",\"tag\":\"top10downloaders\",\"size\":6},{\"name\":\"Worst 10 Round-Trip Time\",\"tag\":\"worst10downloaders\",\"size\":6},{\"name\":\"Worst 10 Retransmits\",\"tag\":\"worst10retransmits\",\"size\":6},{\"name\":\"Top 10 Flows (total bytes)\",\"tag\":\"top10flowsBytes\",\"size\":6},{\"name\":\"Top 10 Flows (rate)\",\"tag\":\"top10flowsRate\",\"size\":6},{\"name\":\"Top 10 Endpoints by Country\",\"tag\":\"top10endpointsCountry\",\"size\":6},{\"name\":\"Ether Protocols\",\"tag\":\"etherProtocols\",\"size\":6},{\"name\":\"IP Protocols\",\"tag\":\"ipProtocols\",\"size\":6},{\"name\":\"CPU Utilization\",\"tag\":\"cpu\",\"size\":3},{\"name\":\"RAM Utilization\",\"tag\":\"ram\",\"size\":3},{\"name\":\"Combined Top 10 Box\",\"tag\":\"combinedTop10\",\"size\":6},{\"name\":\"Total Cake Stats\",\"tag\":\"totalCakeStats\",\"size\":6},{\"name\":\"Circuits At Capacity\",\"tag\":\"circuitCapacity\",\"size\":6},{\"name\":\"Tree Nodes At Capacity\",\"tag\":\"treeCapacity\",\"size\":6}]";
