32d746894a450b96d05b90bdfdd125e89fdc6b34
Herbert Wolverson <herberticus@gmail.com>
2024-07-29 10:38:11 -0500

    Revert the country selection to something that functions.

--- src/rust/lqosd/src/node_manager/js_build/src/asn_explorer.js
+++ src/rust/lqosd/src/node_manager/js_build/src/asn_explorer.js
@@ -192,7 +192,7 @@ function renderAsn(asn, data) {
         // Build the heading
         heading.innerText = "ASN #" + asn.toFixed(0) + " (" + targetAsn.name + ")";
     } else if (renderMode === "country") {
-        let targetCountry = countryList.find((row) => row.name === asn);
+        let targetCountry = countryList.find((row) => row.flag === asn);
         if (targetCountry === undefined || targetCountry === null) {
             console.error("Could not find country: " + asn);
             return;
