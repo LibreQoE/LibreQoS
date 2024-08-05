26c324930ebd6ee10ea818142186a2edf58e0646
Herbert Wolverson <herberticus@gmail.com>
2024-08-04 19:06:00 -0500

    ISSUE #542 - detect if SSL is in use, and adjust the websocket URL to match (either `wss://` or `ws://` for non-TLS).
    The URLs to forward are:
    
    * /websocket/ws (the main "ticker")
    * /websocket/private_ws (for per-instance data feeds) - there's nothing actually "private" about it, it's "session local".

--- src/rust/lqosd/src/node_manager/js_build/src/pubsub/ws.js
+++ src/rust/lqosd/src/node_manager/js_build/src/pubsub/ws.js
@@ -1,12 +1,20 @@
 // Setup any WS feeds for this page
 let ws = null;
 
+export function ws_proto() {
+    if (window.location.protocl === 'https') {
+        return "wss://";
+    } else {
+        return "ws://";
+    }
+}
+
 export function subscribeWS(channels, handler) {
     if (ws) {
         ws.close();
     }
 
-    ws = new WebSocket('ws://' + window.location.host + '/websocket/ws');
+    ws = new WebSocket(ws_proto() + window.location.host + '/websocket/ws');
     ws.onopen = () => {
         for (let i=0; i<channels.length; i++) {
             ws.send("{ \"channel\" : \"" + channels[i] + "\"}");
