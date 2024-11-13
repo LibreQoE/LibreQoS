bddc6c2908541ba4b7991f365fe1ffa53cabd312
Herbert Wolverson <herberticus@gmail.com>
2024-11-13 16:32:22 -0600

    Quick fix because I should know better than to write code when I have to leave!

--- src/rust/lqosd/src/node_manager/js_build/src/dashlets/top10_downloaders.js
+++ src/rust/lqosd/src/node_manager/js_build/src/dashlets/top10_downloaders.js
@@ -47,7 +47,7 @@ export class Top10Downloaders extends BaseDashlet {
             this.buffer.tick();
 
             let results = this.buffer.toArray();
-            results.sort((a, b) => a.bits_per_second.down - b.bits_per_second.down);
+            //results.sort((a, b) => a.bits_per_second.down - b.bits_per_second.down);
 
             let t = TopNTableFromMsgData(results);
 
