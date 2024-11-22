58db8d85356f5cb5c1fd7ccc59c281c5f6d89bb4
Herbert Wolverson <herberticus@gmail.com>
2024-11-22 08:53:19 -0600

    Improved IPv6 prefix check regex

--- src/rust/lqosd/src/node_manager/js_build/src/configuration.js
+++ src/rust/lqosd/src/node_manager/js_build/src/configuration.js
@@ -736,15 +736,9 @@ function checkIpv4(ip) {
 }
 
 function checkIpv6(ip) {
-    const ipv6Pattern =
-        /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
-
-    if (ip.indexOf('/') === -1) {
-        return ipv6Pattern.test(ip);
-    } else {
-        let parts = ip.split('/');
-        return ipv6Pattern.test(parts[0]);
-    }
+    // Check if the input is a valid IPv6 address with prefix
+    const regex = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(\/([0-9]{1,3}))?$/;
+    return regex.test(ip);
 }
 
 function checkIpv4Duplicate(ip, index) {
