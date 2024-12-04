888d50a2a5c40cbb89e133dfaa6ef79371b82d18
Herbert Wolverson <herberticus@gmail.com>
2024-12-04 11:09:31 -0600

    Redo the submission logic for netflow and finished flows to be lock-free.

--- src/rust/lqosd/src/throughput_tracker/flow_data/netflow5/mod.rs
+++ src/rust/lqosd/src/throughput_tracker/flow_data/netflow5/mod.rs
@@ -1,90 +1,86 @@
 //! Support for the Netflow 5 protocol
 //! Mostly taken from: https://netflow.caligare.com/netflow_v5.htm
 mod protocol;
-use super::{FlowAnalysis, FlowbeeLocalData, FlowbeeRecipient};
+use super::{FlowAnalysis, FlowbeeLocalData, };
 use lqos_sys::flowbee_data::FlowbeeKey;
 pub(crate) use protocol::*;
 use std::{
     net::UdpSocket,
-    sync::{atomic::AtomicU32, Arc, Mutex},
+    sync::atomic::AtomicU32,
 };
+use crossbeam_channel::Sender;
 
-pub(crate) struct Netflow5 {
-    socket: UdpSocket,
-    sequence: AtomicU32,
-    target: String,
-    send_queue: Mutex<Vec<(FlowbeeKey, FlowbeeLocalData)>>,
-}
+pub(crate) struct Netflow5 {}
 
 impl Netflow5 {
-    pub(crate) fn new(target: String) -> anyhow::Result<Arc<Self>> {
-        let socket = UdpSocket::bind("0.0.0.0:12212")?;
-        let result = Arc::new(Self {
-            socket,
-            sequence: AtomicU32::new(0),
-            target,
-            send_queue: Mutex::new(Vec::new()),
-        });
-        let thread_result = result.clone();
-        std::thread::Builder::new().name("Netflow5".to_string()).spawn(move || thread_result.queue_handler())?;
-        Ok(result)
-    }
+    pub(crate) fn new(target: String) -> anyhow::Result<Sender<(FlowbeeKey, (FlowbeeLocalData, FlowAnalysis))>> {
+        let (tx, rx) = crossbeam_channel::bounded::<(FlowbeeKey, (FlowbeeLocalData, FlowAnalysis))>(65535);
 
-    fn queue_handler(&self) {
-        loop {
-            let mut lock = self.send_queue.lock().unwrap();
-            if lock.is_empty() {
-                std::thread::sleep(std::time::Duration::from_millis(100));
-                continue;
-            }
+        std::thread::Builder::new()
+            .name("Netflow5".to_string())
+            .spawn(move || {
+                let sequence = AtomicU32::new(0);
+                let mut accumulator = Vec::with_capacity(15);
+                while let Ok((key, (data, analysis))) = rx.recv() {
+                    accumulator.push((key, (data, analysis)));
 
-            let send_chunks = lock.chunks(15);
-            for to_send in send_chunks {
-                let num_records = (to_send.len() * 2) as u16;
-                let sequence = self.sequence.load(std::sync::atomic::Ordering::Relaxed);
-                let header = Netflow5Header::new(sequence, num_records);
-                let header_bytes = unsafe {
-                    std::slice::from_raw_parts(
-                        &header as *const _ as *const u8,
-                        std::mem::size_of::<Netflow5Header>(),
-                    )
-                };
-    
-                let mut buffer = Vec::with_capacity(
-                    header_bytes.len() + to_send.len() * 2 * std::mem::size_of::<Netflow5Record>(),
-                );
-    
-                buffer.extend_from_slice(header_bytes);
-                for (key, data) in to_send {
-                    if let Ok((packet1, packet2)) = to_netflow_5(key, data) {
-                        let packet1_bytes = unsafe {
-                            std::slice::from_raw_parts(
-                                &packet1 as *const _ as *const u8,
-                                std::mem::size_of::<Netflow5Record>(),
-                            )
-                        };
-                        let packet2_bytes = unsafe {
-                            std::slice::from_raw_parts(
-                                &packet2 as *const _ as *const u8,
-                                std::mem::size_of::<Netflow5Record>(),
-                            )
-                        };
-                        buffer.extend_from_slice(packet1_bytes);
-                        buffer.extend_from_slice(packet2_bytes);
+                    let Ok(socket) = UdpSocket::bind("0.0.0.0:12212") else {
+                        tracing::error!("Failed to bind to UDP socket");
+                        continue;
+                    };
+                    if accumulator.len() >= 15 {
+                        Self::queue_handler(&accumulator, &socket, &target, &sequence);
+                        accumulator.clear();
                     }
                 }
+            })?;
 
-                self.socket.send_to(&buffer, &self.target).unwrap();
-                self.sequence.fetch_add(num_records as u32, std::sync::atomic::Ordering::Relaxed);    
-            }
-            lock.clear();
-        }
+        Ok(tx)
     }
-}
 
-impl FlowbeeRecipient for Netflow5 {
-    fn enqueue(&self, key: FlowbeeKey, data: FlowbeeLocalData, _analysis: FlowAnalysis) {
-        let mut lock = self.send_queue.lock().unwrap();
-        lock.push((key, data));
+    fn queue_handler(
+        accumulator: &[(FlowbeeKey, (FlowbeeLocalData, FlowAnalysis))],
+        socket: &UdpSocket,
+        target: &str,
+        sequence: &AtomicU32,
+    ) {
+        loop {
+            let num_records = (accumulator.len() * 2) as u16;
+            let sequence_number = sequence.load(std::sync::atomic::Ordering::Relaxed);
+            let header = Netflow5Header::new(sequence_number, num_records);
+            let header_bytes = unsafe {
+                std::slice::from_raw_parts(
+                    &header as *const _ as *const u8,
+                    std::mem::size_of::<Netflow5Header>(),
+                )
+            };
+
+            let mut buffer = Vec::with_capacity(
+                header_bytes.len() + accumulator.len() * 2 * std::mem::size_of::<Netflow5Record>(),
+            );
+
+            buffer.extend_from_slice(header_bytes);
+            for (key, (data, _)) in accumulator {
+                if let Ok((packet1, packet2)) = to_netflow_5(key, data) {
+                    let packet1_bytes = unsafe {
+                        std::slice::from_raw_parts(
+                            &packet1 as *const _ as *const u8,
+                            std::mem::size_of::<Netflow5Record>(),
+                        )
+                    };
+                    let packet2_bytes = unsafe {
+                        std::slice::from_raw_parts(
+                            &packet2 as *const _ as *const u8,
+                            std::mem::size_of::<Netflow5Record>(),
+                        )
+                    };
+                    buffer.extend_from_slice(packet1_bytes);
+                    buffer.extend_from_slice(packet2_bytes);
+                }
+            }
+
+            socket.send_to(&buffer, target).unwrap();
+            sequence.fetch_add(num_records as u32, std::sync::atomic::Ordering::Relaxed);
+        }
     }
 }
--- src/rust/lqosd/src/throughput_tracker/flow_data/netflow9/mod.rs
+++ src/rust/lqosd/src/throughput_tracker/flow_data/netflow9/mod.rs
@@ -2,72 +2,63 @@ use crate::throughput_tracker::flow_data::netflow9::protocol::{
     header::Netflow9Header, template_ipv4::template_data_ipv4, template_ipv6::template_data_ipv6,
 };
 use lqos_sys::flowbee_data::FlowbeeKey;
-use std::{net::UdpSocket, sync::{atomic::AtomicU32, Arc, Mutex}};
-
+use std::{net::UdpSocket, sync::atomic::AtomicU32};
+use crossbeam_channel::Sender;
 use self::protocol::to_netflow_9;
-use super::{FlowAnalysis, FlowbeeLocalData, FlowbeeRecipient};
+use super::{FlowAnalysis, FlowbeeLocalData};
 mod protocol;
 
-pub(crate) struct Netflow9 {
-    socket: UdpSocket,
-    sequence: AtomicU32,
-    target: String,
-    send_queue: Mutex<Vec<(FlowbeeKey, FlowbeeLocalData)>>,
-}
+pub(crate) struct Netflow9 {}
 
 impl Netflow9 {
-    pub(crate) fn new(target: String) -> anyhow::Result<Arc<Self>> {
+    pub(crate) fn new(target: String) -> anyhow::Result<Sender<(FlowbeeKey, (FlowbeeLocalData, FlowAnalysis))>> {
+        let (tx, rx) = crossbeam_channel::bounded::<(FlowbeeKey, (FlowbeeLocalData, FlowAnalysis))>(65535);
         let socket = UdpSocket::bind("0.0.0.0:12212")?;
-        let result = Arc::new(Self {
-            socket,
-            sequence: AtomicU32::new(0),
-            target,
-            send_queue: Mutex::new(Vec::new()),
-        });
-        let thread_result = result.clone();
-        std::thread::Builder::new().name("Netflow9".to_string()).spawn(move || thread_result.queue_handler())?;
-        Ok(result)
-    }
 
-    fn queue_handler(&self) {
-        loop {
-            let mut lock = self.send_queue.lock().unwrap();
-            if lock.is_empty() {
-                std::thread::sleep(std::time::Duration::from_millis(100));
-                continue;
-            }
+        std::thread::Builder::new()
+            .name("Netflow9".to_string())
+            .spawn(move || {
+                let mut accumulator = Vec::with_capacity(14);
+                let sequence = AtomicU32::new(0);
+                while let Ok((key, (data, analysis))) = rx.recv() {
+                    accumulator.push((key, (data, analysis)));
 
-            let send_chunks = lock.chunks(14);            
-            for to_send in send_chunks {
-                let num_records = (to_send.len() * 2) as u16 + 2; // +2 to include templates
-                let sequence = self.sequence.load(std::sync::atomic::Ordering::Relaxed);
-                let header = Netflow9Header::new(sequence, num_records);
-                let header_bytes = unsafe { std::slice::from_raw_parts(&header as *const _ as *const u8, std::mem::size_of::<Netflow9Header>()) };
-                let template1 = template_data_ipv4();
-                let template2 = template_data_ipv6();
-                let mut buffer = Vec::with_capacity(header_bytes.len() + template1.len() + template2.len() + (num_records as usize) * 140);
-                buffer.extend_from_slice(header_bytes);
-                buffer.extend_from_slice(&template1);
-                buffer.extend_from_slice(&template2);
-
-                for (key, data) in to_send {
-                    if let Ok((packet1, packet2)) = to_netflow_9(key, data) {
-                        buffer.extend_from_slice(&packet1);
-                        buffer.extend_from_slice(&packet2);
+                    if accumulator.len() >= 14 {
+                        Self::queue_handler(&accumulator, &socket, &target, &sequence);
+                        accumulator.clear();
                     }
-                }
-                self.socket.send_to(&buffer, &self.target).unwrap();
-                self.sequence.fetch_add(num_records as u32, std::sync::atomic::Ordering::Relaxed);
-            }
-            lock.clear();
-        }
+                };
+            })?;
 
+        Ok(tx)
     }
-}
 
-impl FlowbeeRecipient for Netflow9 {
-    fn enqueue(&self, key: FlowbeeKey, data: FlowbeeLocalData, _analysis: FlowAnalysis) {
-        let mut lock = self.send_queue.lock().unwrap();
-        lock.push((key, data));
+    fn queue_handler(
+        accumulator: &[(FlowbeeKey, (FlowbeeLocalData, FlowAnalysis))],
+        socket: &UdpSocket,
+        target: &str,
+        sequence: &AtomicU32,
+    ) {
+        loop {
+            let num_records = (accumulator.len() * 2) as u16 + 2; // +2 to include templates
+            let sequence_num = sequence.load(std::sync::atomic::Ordering::Relaxed);
+            let header = Netflow9Header::new(sequence_num, num_records);
+            let header_bytes = unsafe { std::slice::from_raw_parts(&header as *const _ as *const u8, std::mem::size_of::<Netflow9Header>()) };
+            let template1 = template_data_ipv4();
+            let template2 = template_data_ipv6();
+            let mut buffer = Vec::with_capacity(header_bytes.len() + template1.len() + template2.len() + (num_records as usize) * 140);
+            buffer.extend_from_slice(header_bytes);
+            buffer.extend_from_slice(&template1);
+            buffer.extend_from_slice(&template2);
+
+            for (key, (data, _)) in accumulator {
+                if let Ok((packet1, packet2)) = to_netflow_9(key, data) {
+                    buffer.extend_from_slice(&packet1);
+                    buffer.extend_from_slice(&packet2);
+                }
+            }
+            socket.send_to(&buffer, target).unwrap();
+            sequence.fetch_add(num_records as u32, std::sync::atomic::Ordering::Relaxed);
+        }
     }
 }
