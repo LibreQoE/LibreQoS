fd3be877847e2648f7fe4e5e33348e1571e3ee51
Herbert Wolverson <herberticus@gmail.com>
2024-12-11 11:06:08 -0600

    Add base dashlet.

--- helpers/content_builders.js
+++ helpers/content_builders.js
@@ -3,3 +3,11 @@ export function heading5Icon(icon, text) {
     h5.innerHTML = "<i class='fa fa-" + icon + "'></i> " + text;
     return h5;
 }
+
+export function enableTooltips() {
+    // Tooltips everywhere!
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+}
