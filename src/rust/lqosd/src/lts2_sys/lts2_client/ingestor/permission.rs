4264e23a6ebd30964f12780d866aac152a63d1eb
Herbert Wolverson <herberticus@gmail.com>
2025-01-13 11:43:23 -0600

    Force the initial "can I talk to Insight?" to use the HTTPS service and not the old protocol.

--- src/rust/lqosd/src/lts2_sys/lts2_client/ingestor/permission.rs
+++ src/rust/lqosd/src/lts2_sys/lts2_client/ingestor/permission.rs
@@ -1,12 +1,12 @@
-use std::net::TcpStream;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
+use native_tls::TlsConnector;
 use timerfd::{SetTimeFlags, TimerFd, TimerState};
+use tracing::{error, info, warn};
 use uuid::Uuid;
 use lqos_config::load_config;
-use crate::lts2_sys::lts2_client::nacl_blob;
-use crate::lts2_sys::lts2_client::nacl_blob::KeyStore;
+use crate::lts2_sys::lts2_client::license_check::{LicenseRequest, LicenseResponse};
 
 static ALLOWED_TO_SUBMIT: AtomicBool = AtomicBool::new(false);
 
@@ -15,8 +15,6 @@ pub(crate) fn is_allowed_to_submit() -> bool {
 }
 
 pub(crate) fn check_submit_permission() {
-    let keys = Arc::new(KeyStore::new());
-
     let mut tfd = TimerFd::new().unwrap();
     assert_eq!(tfd.get_state(), TimerState::Disarmed);
     tfd.set_state(TimerState::Periodic{
@@ -25,42 +23,66 @@ pub(crate) fn check_submit_permission() {
                   , SetTimeFlags::Default
     );
     
-    check_permission(keys.clone());
+    check_permission();
 
     // Periodically check if we're allowed to submit data
     loop {
         tfd.read();
-        check_permission(keys.clone());
+        check_permission();
     }
 }
 
-fn check_permission(keys: Arc<KeyStore>) {
+fn check_permission() {
     println!("Checking for permission to submit");
     let config = load_config().unwrap();
     let remote_host = {
         config.long_term_stats.lts_url.clone().unwrap_or("insight.libreqos.com".to_string())
     };
-    if let Ok(license_key) = Uuid::parse_str(&config.long_term_stats.license_key.clone().unwrap_or(String::default())) {
-        if let Ok(mut socket) = TcpStream::connect(format!("{}:9122", remote_host)) {
-            if let Err(e) = nacl_blob::transmit_hello(&keys, 0x8342, 3, &mut socket) {
-                println!("Failed to send hello to license server. {e:?}");
-                return;
-            }
+    let license_key = load_config().unwrap().long_term_stats.license_key.clone().unwrap_or_default();
+    let Ok(license_key) = Uuid::parse_str(&license_key) else {
+        warn!("Invalid license key: {license_key}");
+        return;
+    };
+    info!("Checking license key with remote host: {}", remote_host);
+    let url = format!("https://{}/license/license_check", remote_host);
+    info!("License Check URL: {}", url);
+    // Make a ureq request to the remote host. POST a LicenseRequest with the license key.
 
-            if let Ok((server_hello, _)) = nacl_blob::receive_hello(&mut socket) {
-                if let Err(e) = nacl_blob::transmit_payload(&keys, &server_hello.public_key, &license_key, &mut socket) {
-                    println!("Failed to send license key to license server. {e:?}");
-                    return;
-                }
-                if let Ok((response, _)) = nacl_blob::receive_payload::<bool>(&keys, &server_hello.public_key, &mut socket) {
-                    println!("Received response from license server: {response}");
-                    ALLOWED_TO_SUBMIT.store(response, Ordering::Relaxed);
-                    return;
-                } else {
-                    println!("Failed to receive response from license server");
-                }
-            }
-        }
+    let Ok(tls) = TlsConnector::builder()
+        .danger_accept_invalid_certs(true)
+        .danger_accept_invalid_hostnames(true)
+        .build() else {
+        error!("Failed to build TLS connector.");
+        return;
+    };
+    let tls = Arc::new(tls);
+
+    let client = ureq::builder()
+        .timeout_connect(Duration::from_secs(20))
+        .tls_connector(tls.clone())
+        .build();
+
+    let result = client
+        .post(&url)
+        .send_json(serde_json::json!(&LicenseRequest { license: license_key }));
+    if result.is_err() {
+        warn!("Failed to connect to license server. This is not fatal - we'll try again. {result:?}");
+        return;
     }
-    ALLOWED_TO_SUBMIT.store(false, Ordering::Relaxed);
+    let Ok(response) = result else {
+        warn!("Failed to receive license response from license server.");
+        return;
+    };
+    let response = response.into_json::<LicenseResponse>();
+    if response.is_err() {
+        warn!("Failed to receive license response from license server.");
+        return;
+    }
+    let response = response.unwrap();
+    info!("Received license response from license server: {response:?}");
+    if response.valid {
+        ALLOWED_TO_SUBMIT.store(true, Ordering::Relaxed);
+    } else {
+        ALLOWED_TO_SUBMIT.store(false, Ordering::Relaxed);
+    }
 }
