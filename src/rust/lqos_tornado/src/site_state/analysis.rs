8c9153e91c4d27f0389cafdc8faffa7aab558c58
Herbert Wolverson <herberticus@gmail.com>
2025-04-18 15:01:11 -0500

    Fix: More likely to detect rxmit issues

--- src/rust/lqos_tornado/src/site_state/analysis.rs
+++ src/rust/lqos_tornado/src/site_state/analysis.rs
@@ -65,9 +65,9 @@ impl RetransmitState {
             RetransmitState::FallingFast
         } else if tcp_retransmits_relative < 0.8 {
             RetransmitState::Falling
-        }  else if tcp_retransmits_relative > 1.8 {
+        }  else if tcp_retransmits_relative > 1.4 {
             RetransmitState::RisingFast
-        } else if tcp_retransmits_relative > 1.2 {
+        } else if tcp_retransmits_relative > 1.1 {
             RetransmitState::Rising
         } else {
             RetransmitState::Stable
