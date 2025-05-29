cf49be41332a2428b66fffd0fcd09243f4ed9ba1
Herbert Wolverson <herberticus@gmail.com>
2025-05-29 10:56:59 -0500

    Hack: Temporarily suppress the PyO3 warning, because rewriting to use the newer version is going to take a while. Issue #715 created to track the long-term project.

--- src/rust/lqos_python/src/lib.rs
+++ src/rust/lqos_python/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(non_local_definitions)] // Temporary: rewrite required for much of this, for newer PyO3.
 #![allow(unsafe_op_in_unsafe_fn)]
 use lqos_bus::{BlackboardSystem, BusRequest, BusResponse, TcHandle};
 use lqos_utils::hex_string::read_hex_string;
