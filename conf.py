9c0211efb5c368ac543c28879485779f8ef143e6
Robert Chacón <robertchacon@protonmail.com>
2025-10-31 14:16:43 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@ author = "Zach Biles"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = ["myst_parser","replacements"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
