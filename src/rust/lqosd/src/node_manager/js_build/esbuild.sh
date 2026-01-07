778669b1d517fcc284a7c30005fda9b68a117a8c
Herbert Wolverson <herberticus@gmail.com>
2026-01-07 18:30:35 +0000

    Fix: esbuild download is required.

--- src/rust/lqosd/src/node_manager/js_build/esbuild.sh
+++ src/rust/lqosd/src/node_manager/js_build/esbuild.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-ESBUILD_BIN="$(command -v esbuild || true)"
-if [[ -z "$ESBUILD_BIN" ]]; then
+#ESBUILD_BIN="$(command -v esbuild || true)"
+#if [[ -z "$ESBUILD_BIN" ]]; then
   mkdir -p /tmp/esbuild
   pushd /tmp/esbuild
   curl -fsSL https://esbuild.github.io/dl/latest | sh
   popd
   chmod a+x /tmp/esbuild/esbuild
   ESBUILD_BIN="/tmp/esbuild/esbuild"
-fi
+#fi
 scripts=( index.js template.js login.js first-run.js shaped-devices.js tree.js help.js unknown-ips.js configuration.js circuit.js flow_map.js all_tree_sankey.js asn_explorer.js lts_trial.js config_general.js config_tuning.js config_queues.js config_stormguard.js config_lts.js config_iprange.js config_flows.js config_integration.js config_spylnx.js config_netzur.js config_uisp.js config_powercode.js config_sonar.js config_interface.js config_network.js config_devices.js config_users.js config_wispgate.js chatbot.js cpu_weights.js executive_worst_sites.js executive_oversubscribed_sites.js executive_sites_due_upgrade.js executive_circuits_due_upgrade.js executive_top_asns.js executive_heatmap_rtt.js executive_heatmap_retransmit.js executive_heatmap_download.js executive_heatmap_upload.js)
 for script in "${scripts[@]}"
 do
