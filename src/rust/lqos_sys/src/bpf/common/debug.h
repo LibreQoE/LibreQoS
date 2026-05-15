8d8264425b9c8e4b50478f54d9dd97ba74c89596
Herbert Wolverson <herberticus@gmail.com>
2026-05-15 10:53:19 -0500

    Gate BPF debug tracing

--- src/rust/lqos_sys/src/bpf/common/debug.h
+++ src/rust/lqos_sys/src/bpf/common/debug.h
@@ -1,13 +1,17 @@
 #pragma once
 
-// Define VERBOSE if you want to fill
+// Define VERBOSE or TRACING if you want to fill
 // `/sys/kernel/debug/tracing/trace_pipe` with per-packet debug
-// info. You usually don't want this.
+// info. You usually don't want this in production.
 //#define VERBOSE 1
 
+#if defined(VERBOSE) || defined(TRACING)
 #define bpf_debug(fmt, ...)                        \
 	({                                             \
 		char ____fmt[] = " " fmt;             \
 		bpf_trace_printk(____fmt, sizeof(____fmt), \
 						 ##__VA_ARGS__);           \
 	})
+#else
+#define bpf_debug(fmt, ...) ((void)0)
+#endif
