750560a14dff936622557a1e272c2cb26774ed3a
Robert Chacón <robertchacon@protonmail.com>
2026-08-18 09:41:21 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -38,7 +38,7 @@ def ultimateReplace(app, docname, source):
     source[0] = result
 
 ultimate_replacements = {
-    "{deb_url_v1_5}" : "libreqos_2.2.202608181454-1_amd64.deb"
+    "{deb_url_v1_5}" : "libreqos_2.2.202608181534-1_amd64.deb"
 }
 
 def setup(app):
